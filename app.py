5c2ac423f30541c99799c7c5e7055955b27d4c30
Felippe Chemello <felippechemello@gmail.com>
2023-12-08 15:34:46 -0300

    6 & 7 - Setup context window and batch size & get batch randomly from dataset

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import torch
 
 percent_of_training = 0.9
+context_window = 8
+batch_size = 4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 torch.manual_seed(777)
@@ -21,3 +23,17 @@ data = torch.tensor(encode(text), dtype=torch.long)
 n_chars_for_training = int(len(data) * percent_of_training)
 train_data = data[:int(n_chars_for_training)]
 test_data = data[int(n_chars_for_training):]
+
+def get_batch(split): 
+    data = train_data if split == 'train' else test_data
+    split_indexes = torch.randint(len(data) - context_window, (batch_size,))
+    x = torch.stack([data[index:index+context_window] for index in split_indexes])
+    y = torch.stack([data[index+1:index+context_window+1] for index in split_indexes])
+    return x.to(device), y.to(device)
+
+x, y = get_batch('train')
+for batch in range(batch_size):
+    for time in range(context_window):
+        context = x[batch, :time+1]
+        target = y[batch, time]
+        print(f'Context: {context.tolist()} -> Target: {target}')
