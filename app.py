ba4b4ec2d6c9a50d813b1e7577de8b893db319e7
Felippe Chemello <felippechemello@gmail.com>
2023-12-08 15:22:38 -0300

    5 - Split data into train and test

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import torch
 
+percent_of_training = 0.9
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 torch.manual_seed(777)
@@ -17,5 +18,6 @@ encode = lambda string: [char_to_int[char] for char in string]
 decode = lambda ints: ''.join([int_to_char[i] for i in ints])
 
 data = torch.tensor(encode(text), dtype=torch.long)
-print("Data shape: {} - Data type: {}".format(data.shape, data.dtype))
-print("The 1000 character we have seen earlier, the model will look like this: \n{}".format(data[:1000]))
+n_chars_for_training = int(len(data) * percent_of_training)
+train_data = data[:int(n_chars_for_training)]
+test_data = data[int(n_chars_for_training):]
