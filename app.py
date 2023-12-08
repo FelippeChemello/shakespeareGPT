054d2e9636e2fff36510e4040bae2adbca1e6927
Felippe Chemello <felippechemello@gmail.com>
2023-12-08 15:20:25 -0300

    4 - Import torch and encode text into a tensor

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import torch
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+torch.manual_seed(777)
+
 with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
@@ -10,5 +16,6 @@ int_to_char = {i: c for i, c in enumerate(chars)}
 encode = lambda string: [char_to_int[char] for char in string]
 decode = lambda ints: ''.join([int_to_char[i] for i in ints])
 
-print("Encoded: ", encode('Hello World'))
-print("Decoded: ", decode(encode('Hello World')))
+data = torch.tensor(encode(text), dtype=torch.long)
+print("Data shape: {} - Data type: {}".format(data.shape, data.dtype))
+print("The 1000 character we have seen earlier, the model will look like this: \n{}".format(data[:1000]))
