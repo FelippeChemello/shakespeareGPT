78abd27afdaeb5b3b0e08541ac15da929db4b794
Felippe Chemello <felippechemello@gmail.com>
2023-12-08 15:16:07 -0300

    3 - Create encoding and decoding functions and its mapper

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
-print(text[:1000])
-print("O tamanho do texto é de {} caracteres".format(len(text)))
-
 chars = sorted(list(set(text)))
 vocab_size = len(chars)
-print("O texto possui {} caracteres únicos, eles são: {}".format(vocab_size, chars))
+
+char_to_int = {c: i for i, c in enumerate(chars)}
+int_to_char = {i: c for i, c in enumerate(chars)}
+
+encode = lambda string: [char_to_int[char] for char in string]
+decode = lambda ints: ''.join([int_to_char[i] for i in ints])
+
+print("Encoded: ", encode('Hello World'))
+print("Decoded: ", decode(encode('Hello World')))
