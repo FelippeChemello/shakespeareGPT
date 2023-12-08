1d6b7b730e18943447cbdb72235a082ffcff9c9d
Felippe Chemello <felippechemello@gmail.com>
2023-12-08 15:50:33 -0300

    8.2 - Forward function

--- app.py
+++ app.py
@@ -34,7 +34,34 @@ def get_batch(split):
     return x.to(device), y.to(device)
 
 class BigramLanguageModel(nn.Module):
-    def __init__(self, vocab_size):
+    def __init__(self):
         super().__init__()
-        # Cada token lê diretamente os logits do próximo token de um embedding table
+        # Each token directly reads the logits of the next token from an embedding table
         self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+
+    def forward(self, idx, targets=None):
+        # idx and targets are of shape (batch_size, context_window)
+        
+        # The output of the embedding table is of shape (batch_size, context_window, vocab_size)
+        logits = self.token_embedding_table(idx)
+        # Logits are the raw values produced by the last linear layer of a neural network.
+        # Before activation function is applied.
+
+        if targets is None:
+            return logits, None
+        
+        # To calculate the loss we need to reshape the logits and targets to (batch_size * context_window, vocab_size)
+        # https://pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.html 
+        batch_size, context_window_size, vocab_size = logits.shape
+        logits = logits.view(batch_size * context_window_size, vocab_size)
+        targets = targets.view(batch_size * context_window_size)
+
+        loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+    
+model = BigramLanguageModel().to(device)
+x, y = get_batch('train')
+logits, loss = model(x, y)
+print("Logits shape:", logits.shape)
+print("The loss obtained is {:.4f} which is proximate to -log(1/len(vocab)) = {:.4f}".format(loss, -torch.log(torch.tensor(1/vocab_size))))
